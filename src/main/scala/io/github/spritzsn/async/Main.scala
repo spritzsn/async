fd1c03fbac41c60603fde7bb14bd1d0c9b6a7dca
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-18 19:53:09 -0400

    add fs package

--- src/main/scala/io/github/spritzsn/async/Main.scala
+++ src/main/scala/io/github/spritzsn/async/Main.scala
@@ -1,20 +1,20 @@
-package io.github.spritzsn.async
-
-import cps.*
-import cps.monads.FutureAsyncMonad
-
-import scala.concurrent.Future
-import scala.concurrent.duration.*
-import scala.io.Codec
-import scala.util.{Failure, Success}
-
-@main def run(): Unit =
-  async {
-    fs.readFile(".scalafmt.conf", Codec.UTF8) onComplete {
-      case Success(value)     => println(value)
-      case Failure(exception) => println(exception.getMessage)
-    }
-  }
+//package io.github.spritzsn.async
+//
+//import cps.*
+//import cps.monads.FutureAsyncMonad
+//
+//import scala.concurrent.Future
+//import scala.concurrent.duration.*
+//import scala.io.Codec
+//import scala.util.{Failure, Success}
+//
+//@main def run(): Unit =
+//  async {
+//    fs.readFile("asdf", Codec.UTF8) onComplete {
+//      case Success(value)     => println(value)
+//      case Failure(exception) => println(exception.getMessage)
+//    }
+//  }
 
 //  async {
 //    for i <- 1 to 6 do
